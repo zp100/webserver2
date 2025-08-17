94c8c3244963cd6f5c3a3255a8f39e91dbb969a8
Zach Preston <zachattack6089@gmail.com>
2025-08-17 15:28:29 -0700

    Added MySQL database.

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const mysql = require('mysql2/promise')
 
 const app = express();
 
-app.get('/status', (req, res) => {
+app.get('/status', async (req, res) => {
+    const connection = await mysql.createConnection({
+        host: process.env['DB_HOST'],
+        user: process.env['DB_USER'],
+        password: process.env['DB_PASSWORD'],
+        database: process.env['DB_DATABASE'],
+    });
+
+    const [ results, fields ] = await connection.query(`
+        SELECT * from Status LIMIT 1
+    `)
+
     res.send({
-        status: 'Hello world!',
+        status: results[0]['status'],
     })
 })
 
-app.listen(8081);
+app.listen(process.env['PORT'] ?? 8081);
